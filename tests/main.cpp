3916bad13f4bf1b96c05da51359d85727561be0c
LangArthur <arthur.lang.pro@gmail.com>
2022-08-29 08:25:36 +0200

    init test env

---
+++ tests/main.cpp
@@ -0,0 +1,14 @@
+/**
+ * @file main.cpp
+ * @author Arthur Lang (arthur.lang.pro@gmail.Com)
+ * @brief main file for tests
+ * @date 2022-08-29
+ */
+
+#include <gtest/gtest.h>
+
+int main(int argc, char **argv)
+{
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
---
+++ tests/src/test_shader.cpp
@@ -0,0 +1,15 @@
+/**
+ * @file test_shader.cpp
+ * @author Arthur Lang (arthur.lang.pro@gmail.Com)
+ * @brief test Shader object
+ * @date 2022-08-29
+ */
+
+#include <gtest/gtest.h>
+
+class ShaderTest : public testing::Test
+{ };
+
+TEST_F(ShaderTest, compileTest) {
+
+}

0e933c9ae876746accb7083ebbd4170a466dc3f7
LangArthur <arthur.lang.pro@gmail.com>
2022-09-30 18:32:04 +0200

    add possibility to bind a texture to a specific unit.

--- include/Texture.hpp
+++ include/Texture.hpp
@@ -15,18 +15,39 @@ namespace GLTool
     class Texture
     {
         public:
-            Texture(GLenum textureMode, const char *textureFilePath);
-            ~Texture();
+            /**
+             * @brief Construct a new Texture object.
+             * @param textureFilePath path to the texture file.
+             * @param targetTexture mode of the texture (2D, 3D ...)
+             * @param textureFormat format of the data stored into the texture
+             * @param colorSpace color space of texture inside the file
+             */
+            Texture(const char *textureFilePath, GLenum targetTexture, GLenum textureFormat, GLenum colorSpace);
+            ~Texture() = default;
 
-            inline const GLuint id() { return m_id; };
+            inline const GLuint id() const {
+                return m_id;
+            };
+
+            /**
+             * @brief activate the texture to a specific texture unit.
+             * @param textureUnit texture unit id.
+             */
+            void activate(GLenum textureUnit) const;
 
         private:
             GLint m_mipmapLevel = 0;
+            /* type of the current texture */
+            GLenum m_textureType = GL_NONE;
+            /* format store into the texture */
             GLenum m_textureFormat = GL_RGB;
             // texture details
             int m_width = 0;
             int m_height = 0;
             int m_nbrCHannels = 0;
+            /* id of the texture */
             GLuint m_id = 0;
+            /* if the texture has alpha channel*/
+            bool m_isAlpha = false;
     };
 } // namespace GLTool
--- src/Texture.cpp
+++ src/Texture.cpp
@@ -9,11 +9,12 @@
 
 namespace GLTool
 {
-    Texture::Texture(GLenum textureMode, const char *textureFilePath)
+    Texture::Texture(const char *textureFilePath, GLenum targetTexture, GLenum textureFormat, GLenum colorSpace) :
+        m_textureType(targetTexture), m_textureFormat(textureFormat)
     {
         // generate texture
         glGenTextures(1, &m_id);
-        glBindTexture(textureMode, m_id);
+        glBindTexture(m_textureType, m_id);
         // set the texture wrapping/filtering options (on the currently bound texture object)
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);	
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
@@ -23,11 +24,17 @@ namespace GLTool
         // read data from the file
         const auto dataBuffer = stbi_load(textureFilePath, &m_width, &m_height, &m_nbrCHannels, 0);
         if (dataBuffer) {
-            glTexImage2D(textureMode, m_mipmapLevel, m_textureFormat, m_width, m_height, 0, GL_RGB, GL_UNSIGNED_BYTE, dataBuffer);
+            glTexImage2D(m_textureType, m_mipmapLevel, m_textureFormat, m_width, m_height, 0, colorSpace, GL_UNSIGNED_BYTE, dataBuffer);
             glGenerateMipmap(GL_TEXTURE_2D);
         } else {
             Logger::error("Failed to load the texture");
         }
         stbi_image_free(dataBuffer);
+    }
+
+    void Texture::activate(GLenum textureUnit) const
+    {
+        glActiveTexture(textureUnit);
+        glBindTexture(m_textureType, m_id);
     }
 } // namespace GLTool
